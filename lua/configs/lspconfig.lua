a5de540d277216909b0d1f8527328c8fdd8f4843
diego-curiel <diego.curiel.dev@gmail.com>
2026-01-26 08:57:18 -0400

    remove individual lsp configuration

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,38 +1,9 @@
 require("nvchad.configs.lspconfig").defaults()
-local lspconfig = vim.lsp.config
 
-local servers = {
-  ts_ls = {
-    filetypes = {
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-    },
-  },
-  html = {
-    filetypes = {
-      "html",
-      "javascriptreact",
-      "typescriptreact",
-    },
-  },
-  emmet_language_server = {
-    "html",
-    "javascriptreact",
-    "typescript",
-  },
-  cssls = {},
-  pyright = {},
-  intelephense = {
-    filetypes = {
-      "php",
-    },
-  },
+vim.lsp.enable {
+  "biome", --Javascript, Typescript & JSON
+  "emmetls",
+  "jsonls",
+  "luals",
+  "pyright",
 }
-
-for name, opts in pairs(servers) do
-  lspconfig(name, opts)
-end
-
--- read :h vim.lsp.config for changing options of lsp servers
